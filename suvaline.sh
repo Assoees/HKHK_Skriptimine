1a0f2cfee266f3d34860bed44a63c5c9d88d6e07
Assoees <132949073+Assoees@users.noreply.github.com>
2025-09-19 09:23:19 +0300

    Initial commit

---
+++ suvaline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#See on minu esimene bash skript, millest Valdo midagi aru ei saa :)
+
+TERVITUS="See on tervitus AI poolt"
+
+
+echo $TERVITUS
+echo "Sina oled $(whoami)"
+echo "Tore oli sinuga tuttavaks saada!"
+
